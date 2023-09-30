3ad754cb8b33508145e3e837bbec4dc27ff99700
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-09-30 22:23:08 +0700

    1 file modified, 6 added

---
+++ components/footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <van-tabbar route>
+    <van-tabbar-item
+      name="home"
+      to="/home"
+      icon="apps-o"
+    >
+      Home
+    </van-tabbar-item>
+    <van-tabbar-item
+      name="account"
+      to="/account"
+      icon="user-circle-o"
+    >
+      我的
+    </van-tabbar-item>
+  </van-tabbar>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        active: 'home'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
